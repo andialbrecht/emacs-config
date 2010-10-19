931ea2c544a726777b9c47c36d41712f6a59085b
Andi Albrecht <albrecht.andi@gmail.com>
2010-10-19 11:24:13 +0200

    Replace local htmlize by htmlize from ELPA.

--- elpa/htmlize-1.37/htmlize.el
+++ elpa/htmlize-1.37/htmlize.el
@@ -1,4 +1,4 @@
-;; htmlize.el -- Convert buffer text and decorations to HTML.
+;;; htmlize.el --- Convert buffer text and decorations to HTML.
 
 ;; Copyright (C) 1997,1998,1999,2000,2001,2002,2003,2005,2006,2009 Hrvoje Niksic
 
