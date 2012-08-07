db27d01f80c58bafcb6c644e238f5239b3f047c4
Andi Albrecht <albrecht.andi@gmail.com>
2012-08-07 07:52:56 +0200

    Update candy theme.

--- init.el
+++ init.el
@@ -15,15 +15,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(js2-cleanup-whitespace t)
  '(python-indent-guess-indent-offset nil)
- '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "/home/andi/.virtualenvs/lf30ea/bin/python" :settings "settings") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/babbel/bin/python" :settings "settings") (virtualenv-default-directory . "") (virtualenv-workon . "dlgi") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lowfett/bin/python" :settings "settings"))))
+ '(safe-local-variable-values (quote ((ispell-dictionary "german") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lfea/bin/python") (TeX-master . "master") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lf30ea/bin/python" :settings "settings") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/babbel/bin/python" :settings "settings") (virtualenv-default-directory . "") (virtualenv-workon . "dlgi") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lowfett/bin/python" :settings "settings"))))
  '(tab-width 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(highlight ((t (:background "gray14"))))
- '(persp-selected-face ((t (:foreground "olive drab" :weight bold))))
- '(rst-level-1-face ((t (:background "black" :weight bold))) t)
- '(rst-level-2-face ((t (:background "black" :weight bold))) t))
+)
