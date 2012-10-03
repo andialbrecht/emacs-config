6e29156b2f518cb622a644c726d3f3be1a85b14c
Andi Albrecht <albrecht.andi@gmail.com>
2012-10-03 08:38:53 +0200

    Update init.el

--- init.el
+++ init.el
@@ -15,13 +15,34 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(css-indent-offset 2)
+ '(custom-safe-themes (quote ("50c913c6fafd0ba842d9777f1b97c193187a74c792b86c7b38d295e69257c94c" default)))
+ '(ido-ignore-directories (quote ("\\`CVS/" "\\`\\.\\./" "\\`\\./" "\\`\\..*/")))
+ '(ispell-dictionary "american" t)
+ '(js2-basic-offset 2)
  '(js2-cleanup-whitespace t)
+ '(js2-global-externs (list "$"))
+ '(py-electric-comment-p nil)
+ '(py-indent-honors-inline-comment t)
  '(python-indent-guess-indent-offset nil)
- '(safe-local-variable-values (quote ((ispell-dictionary "german") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lfea/bin/python") (TeX-master . "master") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lf30ea/bin/python" :settings "settings") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/babbel/bin/python" :settings "settings") (virtualenv-default-directory . "") (virtualenv-workon . "dlgi") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lowfett/bin/python" :settings "settings"))))
+ '(safe-local-variable-values (quote ((ispell-dictionary . "german") (ispell-dictionary "german") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lfea/bin/python") (TeX-master . "master") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lf30ea/bin/python" :settings "settings") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/babbel/bin/python" :settings "settings") (virtualenv-default-directory . "") (virtualenv-workon . "dlgi") (pony-settings make-pony-project :python "/home/andi/.virtualenvs/lowfett/bin/python" :settings "settings"))))
  '(tab-width 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(ack-match ((t (:background "black" :foreground "gold" :underline (:color foreground-color :style line) :weight bold))) t)
+ '(fixed-pitch ((t (:family "Ubuntu Mono"))))
+ '(font-latex-sectioning-2-face ((t (:inherit font-latex-sectioning-3-face :height 1.0))) t)
+ '(font-latex-sectioning-3-face ((t (:inherit font-latex-sectioning-4-face :height 1.0))) t)
+ '(font-latex-sectioning-4-face ((t (:inherit font-latex-sectioning-5-face :height 1.0))) t)
+ '(font-lock-warning-face ((t (:inherit error :box (:line-width 1 :color "dark red")))))
+ '(highlight ((t (:background "gray14"))))
+ '(hl-line ((t (:inherit highlight :weight bold))))
+ '(monky-diff-add ((t (:foreground "olive drab"))))
+ '(org-todo ((t (:foreground "red" :weight bold))))
+ '(persp-selected-face ((t (:foreground "olive drab" :weight bold))) t)
+ '(rst-level-1-face ((t (:background "black" :weight bold))) t)
+ '(rst-level-2-face ((t (:background "black" :weight bold))) t)
+ '(variable-pitch ((t (:family "Ubuntu Mono")))))
